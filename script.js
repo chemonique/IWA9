999b9b985f18e5beb76f29abac4acf45350dd411
chemonique <122254404+chemonique@users.noreply.github.com>
2023-04-24 12:15:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ const expenses = {
 }
   
 const tax = {
-    734: '3%',
-    234: '20%',
-    913: '12%',
-    415: '38%',
-    502: '42%',
+    734: 3,
+    234: 20,
+    913: 12,            //changed to numbers || change taxAsDecimal to a number with parseInt
+    415: 38,
+    502: 42,
 }
 
 const rent = {
@@ -30,17 +30,11 @@ const rent = {
 
 // You can change below however you want
 
-const taxAsDecimal = parseInt(tax[913]) / 100
+const taxAsDecimal = tax[913] / 100
 const startingAfterTax = salary * (1 - taxAsDecimal)
-const type = lodging + size
-const balance = startingAfterTax- expenses.transport - expenses.food// - expenses.rent.type 
-console.log(balance)
+const type = `${size}-${lodging}`
+const balance = startingAfterTax- expenses.transport - expenses.food - rent[`${type}`]
+console.log(parseFloat(balance).toFixed(2))
 
 
-/* 
-tax = 12/100=0.12
-startingAfterTax= 4000*(1-0.12)=4000*0.88=3520
-type =apartment +large
-balance = startingAfterTax - transport-food-rent
-balance = 3520-10.2-51.7501-800=2658.0499
-*/
+
